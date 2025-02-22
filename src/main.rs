739f515266cf1a74ab5a52a099f9270e250c11d1
Tim Hutt <tdhutt@gmail.com>
2025-02-22 12:44:40 +0000

    Update dependencies to latest versions
    This fixes #2.

--- src/main.rs
+++ src/main.rs
@@ -9,34 +9,34 @@ type BoxErr = Box<dyn std::error::Error>;
 
 fn main() -> Result<(), BoxErr> {
     let matches = Command::new(clap::crate_name!())
-        .arg(Arg::new("filename").takes_value(true).required(true))
+        .arg(Arg::new("filename").num_args(1).required(true))
         .subcommand(
             Command::new("add")
                 .about("Add a new custom section")
-                .arg(Arg::new("section-name").takes_value(true).required(true)),
+                .arg(Arg::new("section-name").num_args(1).required(true)),
         )
         .subcommand(Command::new("list").about("List custom sections"))
         .subcommand(
             Command::new("show")
                 .about("Show a specific custom section")
-                .arg(Arg::new("section-name").takes_value(true).required(true)),
+                .arg(Arg::new("section-name").num_args(1).required(true)),
         )
         .subcommand_required(true)
         .get_matches();
 
-    let filename = matches.value_of("filename").expect("no filename provided");
+    let filename: &String = matches.get_one("filename").expect("no filename provided");
 
     if let Some(matches) = matches.subcommand_matches("add") {
-        let section_name = matches
-            .value_of("section-name")
+        let section_name: &String = matches
+            .get_one("section-name")
             .expect("no section-name provided");
 
         add_custom_section(filename, section_name)
     } else if let Some(_matches) = matches.subcommand_matches("list") {
         list_custom_sections(filename)
     } else if let Some(matches) = matches.subcommand_matches("show") {
-        let section_name = matches
-            .value_of("section-name")
+        let section_name: &String = matches
+            .get_one("section-name")
             .expect("no section-name provided");
 
         show_custom_sections(filename, section_name)
@@ -53,8 +53,8 @@ fn list_custom_sections(filename: &str) -> Result<(), BoxErr> {
 
     for payload in parser.parse_all(&bytes) {
         match payload? {
-            wasmparser::Payload::CustomSection { name, data, .. } => {
-                println!("Section `{}` ({} bytes)", name, data.len());
+            wasmparser::Payload::CustomSection(reader) => {
+                println!("Section `{}` ({} bytes)", reader.name(), reader.data().len());
             }
             _ => {}
         }
@@ -71,10 +71,10 @@ fn show_custom_sections(filename: &str, section_name: &str) -> Result<(), BoxErr
 
     for payload in parser.parse_all(&bytes) {
         match payload? {
-            wasmparser::Payload::CustomSection { name, data, .. } => {
-                if name == section_name {
-                    println!("Section `{}` ({} bytes):", name, data.len());
-                    println!("{}", pretty_hex(&data));
+            wasmparser::Payload::CustomSection(reader) => {
+                if reader.name() == section_name {
+                    println!("Section `{}` ({} bytes):", reader.name(), reader.data().len());
+                    println!("{}", pretty_hex(&reader.data()));
                     return Ok(());
                 }
             }
